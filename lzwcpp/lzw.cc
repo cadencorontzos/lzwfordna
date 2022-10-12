127ecba1084f8242a6195b5b4ba118d823a332e1
cadencorontzos <cadencorontzos@gmail.com>
2022-10-11 21:05:43 -0700

    now we're reading files

--- lzwcpp/lzw.cc
+++ lzwcpp/lzw.cc
@@ -1,10 +1,17 @@
-#include <iostream>
-#include <map>
-#include <string>
 #include "lzw.hh"
 #include "bitio.hh"
 
-using namespace std;
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+#include <chrono>
+#include <filesystem>
+#include <unordered_map>
+
+
+namespace fs = std::filesystem;
+
 
 
 LZW::LZW() = default;
@@ -13,6 +20,44 @@ LZW::LZW() = default;
 LZW::~LZW() = default;
 
 CompressionStatistics* LZW::encode(std::string filename, std::string output_filename){
+    
+    std::ifstream input (filename, std::ios::binary);
+
+    // if file can't be opened, move on to the next argument
+    if (!input.is_open()){
+        std::cout << "Unable to open " << filename << ".\n";
+        return nullptr;
+    }
+
+    std::ofstream output;
+
+    auto original_size =  fs::file_size(filename);
+
+    output.open(output_filename, std::ios::binary);
+
+    std::unordered_map<int, int> dictionary;
+
+    for (int i = 0; i < 256; ++i){
+        dictionary[i] = i;
+    }
+
+    int K;
+    int codeword = 256;
+    std::string str_K;
+    int codeword_size = 8;
+    int max_codeword_size = 8<<1;
+    K = input.get();
+    while (K != -1){
+        
+
+        // get returns -1 if EOF
+        if (K == -1){
+            break;
+        }
+
+        std::cout << char(K);
+        K = input.get();
+    }
     CompressionStatistics* stats = new CompressionStatistics {true, 0.0, 0.0, 1, 1} ;
     return stats;
 }
@@ -25,25 +70,7 @@ CompressionStatistics* LZW::decode(std::string filename, std::string output_file
 
 int main(){
 
-   
-   map<string, float> bakery;
-   bakery["Choclatecake"] = 585.5;
-   bakery["Strawberrycake"] = 579;
-   bakery["Mangocake"] = 345.6;
-   bakery["Vanillacake"] = 650;
-   bakery["MixedFruitcake"] = 539.9;
-
-    map <string,float> :: iterator iter;
-
-   cout<<"keys"<<"  &  "<<"values"<<endl;
-
-   for (iter = bakery.begin(); iter != bakery.end(); iter++)
-   {
-     cout<<(*iter).first<<"    "<<(*iter).second<<"\n";
-   }
-    for(auto i = 0; i < 256; i++){
-        std::cout << std::to_string(i) << std::endl;
-    }
-
-    return 0;
+   std::cout << "Compling at least" << std::endl;
+   auto f = new LZW();
+   f->encode("./test/test.txt", "foo");
 }
