ed7cf626af79ba9cd1388f7b97684527391ce628
cadencorontzos <cadencorontzos@gmail.com>
2022-10-26 15:30:02 -0700

    Script update

--- lzwcpp/integration.sh
+++ lzwcpp/integration.sh
@@ -1,4 +1,4 @@
-./lzwencode testfiles/wap.txt 
-./lzwdecode testfiles/wap.txt.compressed.lzw
+./lzwencode testfiles/wap.txt > /dev/null
+./lzwdecode testfiles/wap.txt.compressed.lzw > /dev/null
 diff testfiles/wap.txt testfiles/wap.txt.compressed.lzw.decompressed.lzw
 rm testfiles/wap.txt.compressed.lzw testfiles/wap.txt.compressed.lzw.decompressed.lzw 
