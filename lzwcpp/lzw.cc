4008768759e2c5f6a1a72c99c43e918514a44658
cadencorontzos <60051669+cadencorontzos@users.noreply.github.com>
2023-04-17 21:32:29 -0700

    fixed bug when chars left = 8

--- lzwcpp/lzw.cc
+++ lzwcpp/lzw.cc
@@ -31,7 +31,6 @@ void LZW::encode(const char *input_file, uint64_t file_size,
   char next_character;
   const char *end_of_input = input_file + file_size;
 
-  /* std::cout << "STARTING " << std::endl; */
   Next_Longest_Run longest_run;
   while (true) {
 
@@ -57,23 +56,19 @@ void LZW::encode(const char *input_file, uint64_t file_size,
         char_bit_o.output_n_bits(encode_values[next_character], 2);
       }
 
-      /* std::cout << "used cw : " << codeword << std::endl; */
 
       // add the run we saw + the new character to our dict
       dictionary.add_string(input_file, longest_run, codeword);
 
       input_file += 8;
     } else {
-      /* std::cout << "found a cw!" << std::endl; */
       indicator_bit_o.output_bit(true);
 
       // output codeword
       codeword_bit_o.output_n_bits(longest_run.codeword_of_next_run,
                                    codeword_helper.bits_per_codeword);
 
-      /* std::cout << longest_run.codeword_of_next_run << " was output" */
       /* << std::endl; */
-      /* std::cout << "It points to"; */
       // output next character
       next_character = input_file[longest_run.next_run_length];
       char_bit_o.output_n_bits(encode_values[next_character], 2);
@@ -150,13 +145,10 @@ void LZW::decode(const char *char_input, const char *codeword_input,
 
     next_is_codeword = indicator_bit_i.input_bit();
 
-    /* std::cout << "next_is_cw" << next_is_codeword << std::endl; */
     if (next_is_codeword) {
-      /* std::cout << "found a cw?" << std::endl; */
 
       codeword_found =
           codeword_bit_i.read_n_bits(codeword_helper.bits_per_codeword);
-      /* std::cout << codeword_found << std::endl; */
       if (codeword_found == codeword_helper.EOF_CODEWORD) {
         break;
       }
@@ -165,13 +157,9 @@ void LZW::decode(const char *char_input, const char *codeword_input,
 
       // look up the codeword in the dictionary and add on next character
       std::string decodedCodeword = dictionary.str_of(codeword_found);
-      /* std::cout << decodedCodeword.length() << std::endl; */
-      /* std::cout << decodedCodeword << std::endl; */
-      assert(decodedCodeword.length() >= 8);
       std::string new_string = std::string();
       new_string = decodedCodeword + next_byte;
 
-      /* std::cout << "it translatest to " << new_string << std::endl; */
       // output the new string
       output << new_string;
 
@@ -184,26 +172,13 @@ void LZW::decode(const char *char_input, const char *codeword_input,
       std::string full_string = std::string();
       for (int i = 0; i < 8; i++) {
         int next_index_value = char_bit_i.read_n_bits(2);
-        assert(next_index_value < 4);
         next_byte = char(decode_values[next_index_value]);
-        if (next_byte != 'A' && next_byte != 'T' && next_byte != 'C' &&
-            next_byte != 'G') {
-          /* std::cout << next_byte; */
-          assert(0);
-        }
         output << next_byte;
         full_string += next_byte;
       }
       Next_Longest_Run nlr = encode_dictionary.find_longest_in_dict(
           full_string.c_str(), full_string.c_str() + full_string.length());
-
-      /* assert( */
-      /*     encode_dictionary.code_of(full_string.c_str(), nlr.next_run_length)
-       * == */
-      /*     nlr.coeword_of_next_run); */
       std::string str_to_add = full_string.substr(0, nlr.next_run_length + 2);
-      /* std::cout << "next_run = " << str_to_add << " " << codeword <<
-       * std::endl; */
       dictionary.add_string(str_to_add, codeword);
       encode_dictionary.add_string(full_string.c_str(), nlr.next_run_length + 1,
                                    codeword);
@@ -221,6 +196,7 @@ void LZW::decode(const char *char_input, const char *codeword_input,
   case 3:
 
     int left = char_bit_i.read_n_bits(3);
+	if(left ==0){left = 8;}
     for (int i = 0; i < left; i++) {
       output << char(decode_values[char_bit_i.read_n_bits(2)]);
     }
